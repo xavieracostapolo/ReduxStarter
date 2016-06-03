46c693ff5e468008e43f0aeea1319ceedcaf71b7
xavieracosta@ingenieros.com <cocojose>
2016-06-03 15:27:45 -0500

    seccion 4, lectura 43

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+    return [
+        { title: 'Javascript: Libro numero 1'},
+        { title: 'Harry Potter'},
+        { title: 'The Dark Tower'},
+        { title: 'Eloquent Ruby'},
+        { title: 'Libro numero 5'}
+    ];
+}
